307fbf9d71982e71b11cc08ccf7b28561dd827cd
Sakina Rao <41284051+sakinarao@users.noreply.github.com>
2023-09-18 12:00:30 -0500

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,48 @@
+export const questions = [
+    {
+      questionText: "How many colors are in the rainbow?",
+      answerOptions: [
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+        { answerText: "10", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Where is Big Ben located?",
+      answerOptions: [
+        { answerText: "Paris", isCorrect: false },
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: true },
+        { answerText: "Washington DC", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the largest bird?",
+      answerOptions: [
+        { answerText: "Emu", isCorrect: false },
+        { answerText: "Ostrich", isCorrect: true },
+        { answerText: "Woodpecker", isCorrect: false },
+        { answerText: "Flamingo", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Which US State is the largest?",
+      answerOptions: [
+        { answerText: "Alaska", isCorrect: true },
+        { answerText: "Texas", isCorrect: false },
+        { answerText: "California", isCorrect: false },
+        { answerText: "Iowa", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the largest animal?",
+      answerOptions: [
+        { answerText: "Elephant", isCorrect: false },
+        { answerText: "Rhinoceros", isCorrect: false },
+        { answerText: "Blue Whale", isCorrect: true },
+        { answerText: "Giraffe", isCorrect: false },
+      ],
+    },
+  ];
+  
